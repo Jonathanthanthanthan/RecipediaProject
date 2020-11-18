74af70696e2028ac7bce2bcdaaa93a06434c73f2
Jonathan <Jonathanaguayoalpha@gmail.com>
2020-11-18 15:01:12 -0800

    changes to database to reflect new users added. Files for login/registration

--- Recipedia/RecipeSearcher/urls.py
+++ Recipedia/RecipeSearcher/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name='RecipeSearcher'
 
 urlpatterns = [
     path('',views.home ),
-    path('login', views.login),
+    path('login', views.userlogin),
     path('results', views.results),
-    path('base', views.base)
-
+    path('base', views.base),
+    path('register', views.register)
 ]
--- Recipedia/RecipeSearcher/views.py
+++ Recipedia/RecipeSearcher/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 def home(request):
     return render(request, 'index.html' )
 
-def login(request):
+def userlogin(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -22,10 +22,25 @@ def login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'userlogin.html', {'form': form})
 
 def results(request):
     return render(request,'results.html')
 
 def base(request):
     return render(request,'base.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request,'register_done.html',{'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,'register.html',{'user_form': user_form})
