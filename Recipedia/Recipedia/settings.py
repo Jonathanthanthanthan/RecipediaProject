99b66c7ad1c8d4c5f3e611501e221d573ae03aea
Jonathan <Jonathanaguayoalpha@gmail.com>
2021-03-17 23:15:33 -0700

    moved requirements.xtx

--- Recipedia/Recipedia/settings.py
+++ Recipedia/Recipedia/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddeware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'Recipedia.urls'
