c4118d8517c6344773539dfecacc71140b78d107
Jonathan <Jonathanaguayoalpha@gmail.com>
2021-03-17 23:06:07 -0700

    added whitenoise to serve static files

--- Recipedia/Recipedia/settings.py
+++ Recipedia/Recipedia/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'dal',
     'dal_select2',
     'RecipeSearcher.apps.RecipesearcherConfig',
-
     'django.contrib.admin',
     'RecipediaPost.apps.RecipediaPostCongig',
     'django.contrib.auth',
@@ -61,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddeware'
 ]
 
 ROOT_URLCONF = 'Recipedia.urls'
