65c7c116633258405436702234fdd2d72c59b49d
Jonathan <Jonathanaguayoalpha@gmail.com>
2021-04-05 13:22:05 -0700

    updated gmail account to use app password

--- Recipedia/Recipedia/settings.py
+++ Recipedia/Recipedia/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
  
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'RecipeSearcher.recipedia@gmail.com'
-EMAIL_HOST_PASSWORD = 'CMPE133!'
+EMAIL_HOST_PASSWORD = 'zyzqemhqjpvixwow'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
